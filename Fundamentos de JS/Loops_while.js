490cc3aee332127b4fd70890b53b8c3460485038
D4SH3R <miguelpaulino239@gmail.com>
2026-01-29 10:49:21 -0300

    Loops While cases

---
+++ Fundamentos de JS/Loops_while.js
@@ -0,0 +1,10 @@
+// While 
+/* while (condition) {
+    // código a ser executado enquanto a condição for verdadeira
+}  */ 
+let contador = 0;
+
+while (contador < 15) {
+    console.log("Contador é: " + contador);
+    contador++;
+}
